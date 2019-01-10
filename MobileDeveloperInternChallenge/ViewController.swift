ae8e5f2f82c98d732333bb7bb011ba7d825f67fd
goshea <goshea@me.com>
2019-01-10 16:28:14 -0500

    Initial Commit

---
+++ MobileDeveloperInternChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileDeveloperInternChallenge
+//
+//  Created by Galen O'Shea on 2019-01-10.
+//  Copyright © 2019 Galen O'Shea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
